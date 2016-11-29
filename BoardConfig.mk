82d4c355c7ee739dc81e55406b9e20178972c310
Deepak Kalgudi <deepak.kalgudi@borqs.com>
2016-11-29 12:41:23 +0530

    Borqs Phone B256 : Initial Changes
    Change-Id: I69389ce9f8323ec6db99f5e78f4b9f97fb182546

---
+++ BoardConfig.mk
@@ -0,0 +1,4 @@
+include device/qcom/msm8909_512/BoardConfig.mk
+
+TARGET_RECOVERY_FSTAB := device/qcom/msm8909_512/recovery.fstab
+TARGET_SYSTEM_PROP := device/qcom/msm8909_512/system.prop
---
+++ b256.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/qcom/msm8909_512/msm8909_512.mk)
+
+PRODUCT_NAME := b256
+PRODUCT_DEVICE := b256
+TARGET_VENDOR := borqs
+PRODUCT_CHARACTERISTICS := phone
